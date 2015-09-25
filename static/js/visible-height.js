4dfbfbde360cbb7f954c9f971e30e08d5c45a655
Rafaël Naciri <raf924@gmail.com>
2015-09-25 11:33:27 +0200

    Added scroll to favourites

---
+++ static/js/visible-height.js
@@ -0,0 +1,12 @@
+(function($) {
+  $.fn.visibleHeight = function() {
+    var elBottom, elTop, scrollBot, scrollTop, visibleBottom, visibleTop;
+    scrollTop = $(window).scrollTop();
+    scrollBot = scrollTop + $(window).height();
+    elTop = this.offset().top;
+    elBottom = elTop + this.outerHeight();
+    visibleTop = elTop < scrollTop ? scrollTop : elTop;
+    visibleBottom = elBottom > scrollBot ? scrollBot : elBottom;
+    return visibleBottom - visibleTop
+  }
+})(jQuery);
