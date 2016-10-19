bd4ac3faf235a6e3a2986f07fd27248704f61b44
Rafael Naciri <rafael.naciri.dev@gmail.com>
2016-10-20 01:23:59 +0200

    Start fix

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 const execFile = require('child_process').execFile;
 const path = require('path');
 var electronPath = 'node_modules/.bin/electron';
-if(process.platform = "win32"){
+if(process.platform == "win32"){
     electronPath += ".cmd";
 }
 const child = execFile(electronPath.replace(/\//g, path.sep),["."], (error, stdout, stderr)=> {
