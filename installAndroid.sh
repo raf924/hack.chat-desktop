1849f626374f81e4aff2ce0852cee6da70d4b85e
Rafael Naciri <rafael.naciri.dev@gmail.com>
2017-02-06 14:43:53 +0100

    Shifting Android build to separate travis instance

--- installAndroid.sh
+++ installAndroid.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#sudo add-apt-repository ppa:openjdk-r/ppa
+#sudo apt-get update
+#sudo apt-get install openjdk-8-jre
+#sudo apt-get install openjdk-8-jdk
 wget https://dl.google.com/android/repository/tools_r25.2.3-linux.zip
 mkdir /android_sdk && unzip tools_r25.2.3-linux.zip -d /android_sdk
 echo y | "$ANDROID_HOME/tools/android" update sdk -u -a -t platform-tools
