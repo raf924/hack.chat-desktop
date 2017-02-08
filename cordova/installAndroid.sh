68174198b10888527d16050777fe8d0573b46287
Rafaël Naciri <ranaciri-ext@airfrance.fr>
2017-02-08 11:47:09 +0100

    Fix Android install

--- cordova/installAndroid.sh
+++ cordova/installAndroid.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 wget https://dl.google.com/android/repository/tools_r25.2.3-linux.zip
-sudo mkdir "$ANDROID_HOME" ;; sudo chmod a+rw "$ANDROID_HOME" && unzip tools_r25.2.3-linux.zip -d "$ANDROID_HOME"
+sudo mkdir "$ANDROID_HOME" ; sudo chmod a+rw "$ANDROID_HOME" && unzip tools_r25.2.3-linux.zip -d "$ANDROID_HOME"
 echo y | "$ANDROID_HOME/tools/android" update sdk -u -a -t platform-tools
 echo y | "$ANDROID_HOME/tools/android" update sdk -u -a -t tools
 echo y | "$ANDROID_HOME/tools/android" update sdk -u -a -t build-tools-25.0.0
