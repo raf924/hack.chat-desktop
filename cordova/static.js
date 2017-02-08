63893d411612a975ad1faeab1261c468c95c6775
Rafael Naciri <rafael.naciri.dev@gmail.com>
2017-02-08 23:17:51 +0100

    Fix android build

--- cordova/static.js
+++ cordova/static.js
@@ -2,7 +2,7 @@ require('./../node_modules/roboto-fontface/css/roboto/roboto-fontface.css');
 require('material-components-web/dist/material-components-web.min.css');
 require('./../node_modules/material-design-icons/iconfont/material-icons.css');
 require('./../index.html');
-let r = require.context(`${__dirname}/static/css`);
+let r = require.context(`${__dirname}/../static/css`);
 r.keys().forEach((file) => {
     r(file);
 });
